21126a576496d43d98bcdd74d8235995ae610e52
Konrad <konrad@Konrads-MBP.lan>
2020-09-18 10:29:41 +0200

    finished version

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace assignment_two_hangman
 {
@@ -17,7 +18,7 @@ namespace assignment_two_hangman
 
         static Char[] AskUserForLetter()
         {
-            Console.WriteLine("Guess a letter or word, and press enter.");
+            Console.WriteLine("Guess a letter or a word, and press enter.");
             Console.Write("Your choice: ");
             String word = new String(Console.ReadLine());
             Char[] guess = word.ToCharArray();
@@ -37,71 +38,143 @@ namespace assignment_two_hangman
                 return hiddenLetters;
         }
 
+        
+
         static void Main(string[] args)
         {
             Console.WriteLine("The Hangman Game!");
             var getRandomWord = RandomWord();
-            Console.WriteLine(getRandomWord);
+            //Console.WriteLine(getRandomWord);
             Console.WriteLine(HiddenRandomWord(getRandomWord));
             var hiddenPlayWord = HiddenRandomWord(getRandomWord);
             
-            //var letter = AskUserForLetter();
             var lives = 10;
-        
+            var counter = 0;
+            StringBuilder builder = new StringBuilder();
+            var guess = AskUserForLetter();
+            builder.Append(guess);
+            Console.WriteLine("Your guesses: {0}", builder);
+            var previousGuesses = " ";
+            String[] guessStringArray = new String[10];
+            char[] delimiter1 = new char[] { ',', ' ' };
+
+            
+           
             bool keepLooping = true;
             while(keepLooping)
             {   
-                
-                
-                var guess = AskUserForLetter();
+                if(lives == 0)
+                    {
+                        Console.WriteLine("You are out of lives.");
+                        keepLooping = false;
+                        break;
+                      
+                    } 
+
+                bool keepLooping2 = true;
+                while(keepLooping2 && counter >= 1)
+                {
+                    var newGuess = AskUserForLetter();
+                    
+                    previousGuesses = builder.ToString();
+                    
+                    String newGuessToString = new String(newGuess);
+
+                    String[] array1 = previousGuesses.Split(delimiter1, StringSplitOptions.None);
+
+                    if(newGuessToString.Length <= 1)
+                    {
+                        for(var i = 0; i < array1.Length; i++)
+                        {
+                            if(array1[i] != newGuessToString)
+                            {
+                                
+                                guess = newGuess;
+                                keepLooping2 = false;
+                                continue;
+                            } else
+                            {
+                                Console.WriteLine("You have already guessed this letter.");
+                                keepLooping2 = true;
+                                
+                            }
+                        }
+                        builder.Append(", ");
+                        builder.Append(newGuess);
+                        Console.WriteLine("Your guesses: {0}", builder);
+
+                    } else if(!getRandomWord.SequenceEqual(guess) && guess.Length > 1)
+                    {
+                        for(var i = 0; i < array1.Length; i++)
+                        {
+                            if(array1[i] == newGuessToString)
+                            {
+                                Console.WriteLine("You have already guessed this word.");
+                                continue;
+                            }
+                        }
+                    } else
+                    {
+                        builder.Append(", ");
+                        builder.Append(newGuess);
+                        Console.WriteLine("Your guesses: {0}", builder);
+                        guess = newGuess;
+                        keepLooping2 = false;
+                    }
+                }
+                counter++;
                 lives--;
-                Console.WriteLine("Lives left: {0}", lives);
-                if(hiddenPlayWord.SequenceEqual(guess))
+                Console.WriteLine("Your lives: {0}", lives);
+
+                if(getRandomWord.SequenceEqual(guess))
                 {
                     Console.WriteLine("You got it!");
                     keepLooping = false;
+                    break;
+                   
+                } 
+
+                if(!getRandomWord.SequenceEqual(guess) && guess.Length > 1)
+                {
+                    Console.WriteLine("Wrong word");
+                    keepLooping = true;
+                    continue;
                 }
-                
-                
-                keepLooping = false;
+
+
+            
                 for(int i = 0; i < getRandomWord.Length; i++)
                 {
                     
                     for(int j = 0; j <guess.Length; j++)
                     {
-                        
-                    
-                    if(lives == 10)
-                    {
-                        Console.WriteLine("You are out of lives.");
-                        keepLooping = false;
-
-                    } else if(hiddenPlayWord.SequenceEqual(guess))
-                    {
-                            Console.WriteLine("You made it!");
-                            keepLooping = false;
-
-                    } else if(guess[j] == getRandomWord[i])
+                      
+                    if(guess[j] == getRandomWord[i])
                     {
                         hiddenPlayWord[i] = guess[j];
                         
-                        
                         keepLooping = true;
-            
+                        
+                        if(getRandomWord.SequenceEqual(hiddenPlayWord))
+                        {
+                            Console.WriteLine("You made it!");
+                            keepLooping = false;
+                            break;
+                        }
+
                     } else if(guess[j] != getRandomWord[i])
                     {
+                        keepLooping = true;
                         
-                        
+                    } else 
+                    {
                         keepLooping = true;
                     }
                     }
                 } Console.WriteLine(hiddenPlayWord);
-            }
-
-           
-            
-
-        
+                    
+                    
+            } 
 
         }
     }
